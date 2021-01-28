7a29bef5a5330881fd84fcadb39fbea6230960ff
mattia-vincenzi <m.vincenzi14@campus.unimib.it>
2021-01-28 17:32:58 +0100

    Removed unused import

--- SpeedLayer/KafkaSubredditDataConsumer/src/main/java/it/unimib/lsdm/subredditanalyzer/KafkaSubredditDataConsumer/handler/MessageHandlerThreadsData.java
+++ SpeedLayer/KafkaSubredditDataConsumer/src/main/java/it/unimib/lsdm/subredditanalyzer/KafkaSubredditDataConsumer/handler/MessageHandlerThreadsData.java
@@ -22,7 +22,6 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.LinkedHashSet;
-import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
